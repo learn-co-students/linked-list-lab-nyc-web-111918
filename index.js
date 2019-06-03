bf7e6c26a3ea0151fb4e4e6bcf4f27e3977194bb
lumrachele <rlum894@gmail.com>
2019-06-02 22:35:23 -0400

    done

--- index.js
+++ index.js
@@ -29,5 +29,38 @@ function addressAt(index, linkedList, collection){
 }
 
 function indexAt(node, collection, linkedList){
-  
+  let current = headNode(linkedList, collection)
+  let counterIndex = 0
+  while(current !== node){
+    counterIndex++
+    current = next(current, collection)
+  }
+  return counterIndex
+}
+
+function insertNodeAt(index, newAddress, linkedList, collection){
+  let prev = nodeAt(index-1, linkedList, collection)
+  let next = nodeAt(index, linkedList, collection)
+
+  let prevInd = indexAt(prev, collection, linkedList)
+  let nextInd = indexAt(next, collection, linkedList)
+
+  let prevAddress = addressAt(prevInd, linkedList, collection)
+  let nextAddress = addressAt(nextInd, linkedList, collection)
+
+  prev.next = newAddress
+  let newNode = collection[newAddress]
+
+  newNode.next = nextAddress
+}
+
+function deleteNodeAt(ind, linkedList, collection){
+  let prevNode;
+  let currentNode = headNode(linkedList, collection)
+
+  for(let i=0; i<ind; i++){
+    prevNode = currentNode
+    currentNode = next(currentNode, collection)
+  }
+  prevNode.next = currentNode.next
 }
